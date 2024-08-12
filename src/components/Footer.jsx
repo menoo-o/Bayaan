cb7da516ae0bea2b12570851e998505ed653716d
menoo-o <menosuper6@gmail.com>
2024-08-12 14:31:01 +0500

    updated UI

---
+++ src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import '../App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faYoutube,
+  faSpotify,
+  faSoundcloud
+} from '@fortawesome/free-brands-svg-icons';
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className= "footer-container">
+            <div className='links'>
+                <a href='#'>Home</a>
+                <a href='#'>Shows</a>
+                <a href='#'>Merch</a>
+                <a href='#'>Subscribe</a>
+            </div>
+
+
+            <div className="social-media">
+            <a href='#'><FontAwesomeIcon icon={faFacebook} /></a>
+            <a href='#'><FontAwesomeIcon icon={faInstagram} /></a>
+            <a href='#'><FontAwesomeIcon icon={faTwitter} /></a>
+            <a href='#'><FontAwesomeIcon icon={faYoutube} /></a>
+            <a href='#'><FontAwesomeIcon icon={faSpotify} /></a>
+            <a href='#'><FontAwesomeIcon icon={faSoundcloud} /></a>
+            </div>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
