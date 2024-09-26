0a665ae74cfc63b526634424631324cd7e088139
menoo-o <menosuper6@gmail.com>
2024-09-26 22:35:44 +0500

    netlify error fix1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-import Navbar from "./Components/Navbar";
+import Navbar from './Components/Navbar'
 import Footer from './Components/Footer';
 
 // pages
