81ba3df4e17221bd23f83d9a8330df20289c84a4
menoo-o <menosuper6@gmail.com>
2024-09-26 21:26:19 +0500

    landing page done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,8 +16,25 @@ const Footer = () => {
 
   return (
     <>
-
+        
         <div className= "footer-container">
+
+          <div className="social-media">
+
+                <div className='fb-insta-x'>
+                  <a href='#'><FontAwesomeIcon icon={faFacebook} /></a>
+                  <a href='#'><FontAwesomeIcon icon={faInstagram} /></a>
+                  <a href='#'><FontAwesomeIcon icon={faTwitter} /></a>
+                </div>
+
+                <div className='music-platforms'>
+                  <a href='#'><FontAwesomeIcon icon={faYoutube} /></a>
+                  <a href='#'><FontAwesomeIcon icon={faSpotify} /></a>
+                  <a href='#'><FontAwesomeIcon icon={faSoundcloud} /></a>
+                </div>
+                
+              </div>
+
             <div className='links'>
                 <a href='#'>Home</a>
                 <a href='#'>Shows</a>
@@ -26,14 +43,7 @@ const Footer = () => {
             </div>
 
 
-            <div className="social-media">
-            <a href='#'><FontAwesomeIcon icon={faFacebook} /></a>
-            <a href='#'><FontAwesomeIcon icon={faInstagram} /></a>
-            <a href='#'><FontAwesomeIcon icon={faTwitter} /></a>
-            <a href='#'><FontAwesomeIcon icon={faYoutube} /></a>
-            <a href='#'><FontAwesomeIcon icon={faSpotify} /></a>
-            <a href='#'><FontAwesomeIcon icon={faSoundcloud} /></a>
-            </div>
+         
         </div>
     </>
   )
