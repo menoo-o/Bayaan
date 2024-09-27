ecdb46346ab3eee7a64d50f5da3e25235e184aaa
menoo-o <menosuper6@gmail.com>
2024-09-27 12:07:15 +0500

    final commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Bayaan
 
-This is a static website for the Pakistani rock band Bayaan, implemented as a Client-Side Rendered (CSR) React Single Page Application (SPA). This project aims to provide a modern, responsive, and informative online presence for the band.
+This is a static website for the Pakistani rock band Bayaan, implemented as a Client-Side Rendered (CSR) React Single Page Application (SPA). This project aims to provide a modern, responsive, and informative online presence for the band. 
+
+<p>This website only contains two page, landing & shows will add more if i get the contract from the band.</p>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 const Hero = () => {
+  const [image, setImage] = useState('');
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 1200) {
+        setImage('hero.jpg');
+      } else {
+        setImage('hero2.jpg');
+      }
+    };
+
+    // Run on initial mount to set the correct image based on current screen width
+    handleResize();
+
+    // Add resize event listener
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup function to remove event listener
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className='hero-container'>
-      <img src='hero2.jpg' alt='hero image' className='hero-img'/>
+      <img src={image} alt='hero image' className='hero-img' />
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
